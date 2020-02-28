aaba8ac821967473b1d524325a826fb7152bd608
Zhazira <g.zhazira@gmail.com>
2020-02-28 10:26:59 +0500

    add RDS instance

--- StartDeutsch/settings.py
+++ StartDeutsch/settings.py
@@ -80,14 +80,40 @@ WSGI_APPLICATION = 'StartDeutsch.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+# LOCAL DB
+"""
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "database",
+        "USER": "zhazira",
+        "PASSWORD": "nescafe",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+"""
+# RDS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbstartdeutsch',
+        'USER': 'postgres',
+        'PASSWORD': 'NescafeGold2454',
+        'HOST': 'dbinstance1.colaotafqlzu.us-east-2.rds-preview.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
