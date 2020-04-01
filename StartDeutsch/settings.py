92cf7901ae9d5334b8fb724ae024c28b3ad70b1c
Zhazira <g.zhazira@gmail.com>
2020-04-01 12:24:38 +0500

    fix typo

--- StartDeutsch/settings.py
+++ StartDeutsch/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*',
 
 # Application definition
 
-INSTALLED_APPS = [GIT
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
